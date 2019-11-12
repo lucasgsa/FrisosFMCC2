b77bde5bf23e8e0244666e9f77b2e8c4e9675b1a
Lucas Gabriel KPNz <48533541+lucasgsa@users.noreply.github.com>
2019-11-11 22:02:58 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -15,6 +15,27 @@ def reflexaoHorizontal(friso):
 	friso1 = friso.reflexaoHorizontal()
 	return friso.addBaixo(friso1)
 	
+def darMeiaVolta(friso):
+	tempMatriz1 = numpy.array(friso.imagem)
+	matrizVazia = tempMatriz1.copy()
+	for i in range(len(matrizVazia)):
+		for j in range(len(matrizVazia[i])):
+			if matrizVazia[i][j].tolist() != [255, 255, 255]:
+				matrizVazia[i][j] = numpy.array([255,255,255])
+	novo = numpy.concatenate((tempMatriz1, matrizVazia),axis=1)
+	return addBaixo(Friso(Image.fromarray(novo)), (Friso(Image.fromarray(novo)).reflexaoVertical().reflexaoHorizontal()))
+	
+def addDireita(friso, outro):
+	tempMatriz1 = numpy.array(friso.imagem)
+	tempMatriz2 = numpy.array(outro.imagem)
+	new = numpy.concatenate((tempMatriz1, tempMatriz2),axis=1)
+	return Friso(Image.fromarray(new))
+def addBaixo(friso, outro):
+	tempMatriz1 = numpy.array(friso.imagem)
+	tempMatriz2 = numpy.array(outro.imagem)
+	new = numpy.concatenate((tempMatriz1, tempMatriz2))
+	return Friso(Image.fromarray(new))
+	
 class Friso():
 	def __init__(self, imagem):
 		self.imagem = imagem
@@ -22,15 +43,10 @@ class Friso():
 		return self.imagem
 	def salvarImagem(self, caminho):
 		self.imagem.save(caminho)
+	def addDiagonal(self, outro):
+		tempMatriz1 = numpy.array(self.imagem)
+		tempMatriz2 = numpy.array(outro.imagem)
 		
-	def addDireita(self, outro):
-		tempMatriz1 = numpy.array(self.imagem)
-		tempMatriz2 = numpy.array(outro.imagem)
-		new = numpy.concatenate((tempMatriz1, tempMatriz2),axis=1)
-		return Friso(Image.fromarray(new))
-	def addBaixo(self, outro):
-		tempMatriz1 = numpy.array(self.imagem)
-		tempMatriz2 = numpy.array(outro.imagem)
 		new = numpy.concatenate((tempMatriz1, tempMatriz2))
 		return Friso(Image.fromarray(new))
 	def reflexaoVertical(self):
@@ -49,15 +65,18 @@ imagem = abreImagem("pe.png")
 quantidadeRepeticoes = 1
 
 friso = Friso(imagem)
-friso1 = friso.addDireita(friso)
-friso2 = friso.addDireita(friso.reflexaoHorizontal())
-friso3 = friso.addDireita(friso.reflexaoVertical())
-friso4 = friso.addBaixo(friso.reflexaoHorizontal())
-friso5 = friso.addDireita(friso.reflexaoHorizontal().reflexaoVertical())
-friso6 = friso.addDireita(friso.reflexaoVertical()).addDireita(friso.addDireita(friso.reflexaoVertical()).reflexaoHorizontal())
-friso7 = friso.addDireita(friso.reflexaoVertical()).addBaixo(friso.addDireita(friso.reflexaoVertical()).reflexaoHorizontal())
 
-listaDeFrisos = [friso1, friso2, friso3, friso4, friso5, friso6, friso7]
+friso1 = darMeiaVolta(friso)
+
+friso1.imagem.show()
+#friso2 = friso.addDireita(friso.reflexaoHorizontal())
+#friso3 = reflexaoVertical(friso.reflexaoVertical())
+#friso4 = reflexaoHorizontal(friso)
+#friso5 = friso.addDireita(friso.reflexaoHorizontal().reflexaoVertical())
+#friso6 = reflexaoVertical(friso).addDireita(reflexaoVertical(friso).reflexaoHorizontal())
+#friso7 = reflexaoHorizontal(reflexaoVertical(friso))
+
+listaDeFrisos = [friso1]
 
 for i in range(len(listaDeFrisos)):
 	for j in range(quantidadeRepeticoes):
